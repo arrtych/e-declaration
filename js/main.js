ad3e3414905c7b7b0d24035b63a32ebb78fa4fe2
Artur Lipin <artur.lipin@ttu.ee>
2017-03-14 22:20:30 +0200

    validate v2

--- js/main.js
+++ js/main.js
@@ -1,34 +1,40 @@
-  // $('#main-form [type="submit"]').click(function(){
-  //     // выбираем все невидимые обязательные поля
-  //     $('input, textarea, select').filter('[required]:hidden').each(function(){
-  //         // проверяем их заполненность
-  //         if (!$(this)[0].checkValidity()) {
-  //             var tabId = $(this).closest('.tab-pane').attr('id');
-  //             // переключаем таб
-  //             $('.nav-tabs a[href="#' + tabId + '"]').tab('show');
-  //             // прерываем обработку остальных обязательных полей
-  //             return false;
-  //         }
-  //     });
-  //         console.log("a");
-  // });
   function validateForm(){
-      var x=document.forms["f1"]["inputEesnimi"].value;
-      var y=document.forms["f1"]["inputPerekonnanimi"].value;
-      if (x==null || x==""){
-        // alert("Sisesta eesnimi");
-        // document.forms["f1"]["inputEesnimi"].focus();
+      var a=document.forms["f1"]["inputEesnimi"].value;
+      var b=document.forms["f1"]["inputPerekonnanimi"].value;
+      var c=document.forms["f1"]["inputIsikukood"].value;
+      var d=document.forms["f1"]["inputSunniaeg"].value;
+      var e=document.forms["f1"]["inputTelefon"].value;
+      // Check eesnimi
+      if (a==null || a==""){
+        document.forms["f1"]["inputEesnimi"].focus();
         $(".eesnimi-v").addClass("has-error");
         $("#inputEesnimi").attr("placeholder", "Sisesta eesnimi");
         return false;
+      }else{
+        $(".eesnimi-v").addClass("has-success");
       }
- 
-      if(y==null || y==""){
-        // alert("Введите isikukood");
-        // document.forms["f1"]["inputPerekonnanimi"].focus();
+      // Check perekonnanimi
+      if(b==null || b==""){
+        document.forms["f1"]["inputPerekonnanimi"].focus();
         $(".perekonnanimi-v").addClass("has-error");
         $("#inputPerekonnanimi").attr("placeholder", "Sisesta perekonnanimi");
         return false;
+      }else{
+        $(".perekonnanimi-v").addClass("has-success");
+      }
+      // if((c==null || c=="")|| (d==null || d=="")){
+      if(c==null || c==""){
+        alert("Sisesta isikukood või sünniaeg");
+        document.forms["f1"]["inputIsikukood"].focus();
+        document.forms["f1"]["inputSunniaeg"].focus();
+        $(".isikukood-v").addClass("has-error");
+        $(".sunniaeg-v").addClass("has-error");
+        $("#inputIsikukood").attr("placeholder", "Sisesta isikukood");
+        $("#inputSunniaeg").attr("placeholder", "Sisesta snniaeg");
+        return false;
+      }else{
+        $(".isikukood-v").addClass("has-success");
+        $(".sunniaeg-v").addClass("has-success");
       }
       
     return true;  
